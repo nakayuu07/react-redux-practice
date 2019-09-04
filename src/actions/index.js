b2032dd975fec59aa6f8b3d7f7e607adb680928b
nakayuu07 <nakayuu07@gmail.com>
2019-09-04 18:03:44 +0900

    Minor Correction

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,12 +1,10 @@
-export const COUNT_UP = "COUNT_UP"
-export const COUNT_DOWN = "COUNT_DOWN"
+import axios from 'axios'
 
-export const countUp = () => {
-  return { type: COUNT_UP }
+export const READ_EVENTS = "READ_EVENTS"
+const ROOT_URL = 'https://udemy-utils.herokuapp.com/api/v1'
+const QUERYSTRING = '?token=token123'
+
+export const readEvents = () => async dispatch => {
+  const responce = await axios.get(`${ROOT_URL}/events${QUERYSTRING}`)
+  dispatch({ type: READ_EVENTS, responce })
 }
-
-export const countDown = () => {
-  return { type: COUNT_DOWN }
-}
-
-
