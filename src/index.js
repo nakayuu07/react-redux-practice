163b2bf89f2c608871e9ddf3bbcfb2ad33dafc14
nakayuu07 <nakayuu07@gmail.com>
2019-09-04 13:47:07 +0900

    add: Store

--- src/index.js
+++ src/index.js
@@ -1,8 +1,20 @@
 import React from 'react';
 import { render } from 'react-dom';
-import Counter from './Counter';
+
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+
+import Counter from './components/Counter';
+import reducer from './reducers'
 import * as serviceWorker from './serviceWorker';
 
-render(<Counter />, document.getElementById('root'));
+const store = createStore(reducer)
+
+render(
+  <Provider store={store}>
+    <Counter />
+  </Provider>,
+  document.getElementById('root')
+);
 
 serviceWorker.unregister();
