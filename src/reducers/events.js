5b38c86b61d76d0fd4aa24a5609a4de23e6c08e6
nakayuu07 <nakayuu07@gmail.com>
2019-09-05 13:37:22 +0900

    fix: state

--- src/reducers/events.js
+++ src/reducers/events.js
@@ -1,10 +1,9 @@
-import _ from 'lodash'
 import { READ_EVENTS } from '../actions'
 
-const events = (events = {}, action) => {
+const events = (events = [], action) => {
   switch (action.type) {
     case READ_EVENTS:
-      return (_.mapKeys(action.responce.data, 'id'))
+      return [...action.responce.data]
     default:
       return events
   }
