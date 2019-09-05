21a10a16bad4e5c39cc5a12ecab0ea89db60e15d
nakayuu07 <nakayuu07@gmail.com>
2019-09-05 14:36:08 +0900

    add: routes

--- src/index.js
+++ src/index.js
@@ -4,8 +4,10 @@ import { render } from 'react-dom';
 import { createStore, applyMiddleware } from 'redux'
 import { Provider } from 'react-redux'
 import thunk from 'redux-thunk'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
 
 import Events from './components/Events';
+import EventsNew from '.components/EventsNew'
 import reducer from './reducers'
 import * as serviceWorker from './serviceWorker';
 
@@ -13,7 +15,12 @@ const store = createStore(reducer, applyMiddleware(thunk))
 
 render(
   <Provider store={store}>
-    <Events />
+    <BrowserRouter>
+      <Switch>
+        {/* <Route exact path='/events/new' component={EventsNew} /> */}
+        <Route exact path='/' component={Events} />
+      </Switch>
+    </BrowserRouter>
   </Provider>,
   document.getElementById('root')
 );
